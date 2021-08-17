59b26cadc6d735be7c81a793f7ef7e8e0b8c7bfc
Grégoire Antoine <he201703@students.ephec.be>
2021-08-17 14:24:40 +0200

    Config docker + DNS

--- web/b2b/index.php
+++ web/b2b/index.php
@@ -5,7 +5,7 @@ $password = "admin";
 $database = "woodtoys_db";
 $table = "inventaire";
     try {
-  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+  $db = new PDO("mysql:host=db-internal;dbname=$database", $user, $password);
   echo "<h2>Site de b2b en php</h2><ol>";
   foreach($db->query("SELECT *  FROM $table") as $row) {
     echo "<li>" . $row['Name'] ." ".$row['Price']."€ "."</li>";
